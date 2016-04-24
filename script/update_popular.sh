bf5792923617e2ea0655251c0e360c10bee5d7e8
wisonlin <460760604@qq.com>
2016-04-24 23:37:58 +0800

    update popular

--- script/update_popular.sh
+++ script/update_popular.sh
@@ -1,8 +1,7 @@
 
 # fetch popular
 
-$url=https://api.dribbble.com/v1/shots?sort=&access_token=6146bef0c11604a7d27f6809a4e6550cd8681e68410492565348c9ab93fa8da6
-curl $url > ../data/popular.json
+curl https://api.dribbble.com/v1/shots?sort=&access_token=6146bef0c11604a7d27f6809a4e6550cd8681e68410492565348c9ab93fa8da6 > ../data/popular.json
 
 # commit to github
 
