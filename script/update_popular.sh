eaef89406f1a169a2b0f706b30f60b2ef82b320c
wisonlin <460760604@qq.com>
2016-04-24 23:45:14 +0800

    update popular

--- script/update_popular.sh
+++ script/update_popular.sh
@@ -1,7 +1,8 @@
 
 # fetch popular
 
-curl 'https://api.dribbble.com/v1/shots?sort=&access_token=6146bef0c11604a7d27f6809a4e6550cd8681e68410492565348c9ab93fa8da6' > ../data/popular.json
+curl 'https://api.dribbble.com/v1/shots?sort=&access_token=6146bef0c11604a7d27f6809a4e6550cd8681e68410492565348c9ab93fa8da6' > ../data/popular.json.1
+curl 'https://api.dribbble.com/v1/shots?sort=&page=2&access_token=6146bef0c11604a7d27f6809a4e6550cd8681e68410492565348c9ab93fa8da6' > ../data/popular.json.2
 
 # commit to github
 
